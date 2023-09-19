f60beda99670ef735ac5f160b2d063dc50d33d55
FranciscoSMP <fmoralesp2001@gmail.com>
2023-09-19 00:50:37 -0600

    19/09/2023

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ import os
 
 locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
 
-while True:
+fecha_actual = datetime.datetime.now()
+year = fecha_actual.year
+month = fecha_actual.month
 
+
+def mostrar_calendario(year, month):
+    
+    os.system('cls')
+    
     print('\n\t\t============')
     print('\t\t   AGENDA')
     print('\t\t============')
-
-    fecha_actual = datetime.datetime.now()
-
-    year = fecha_actual.year
-    month = fecha_actual.month
+    
     cal = calendar.month(year, month)
-
     cal_lines = cal.splitlines()
 
     nombre_mes = cal_lines[0].split()[0].capitalize() + ' ' + cal_lines[0].split()[1]
@@ -30,31 +32,8 @@ while True:
     print('\t1. Siguiente')
     print('\tx. Salir')
 
-    op = input('\n\t').lower()
-    
-    if op == '0':
-        
-        os.system('cls')
-        
-        print('\n\t\t============')
-        print('\t\t   AGENDA')
-        print('\t\t============')
-        
-        cal = calendar.month(year, month-1)
-        
-        cal_lines = cal.splitlines()
-
-        nombre_mes = cal_lines[0].split()[0].capitalize() + ' ' + cal_lines[0].split()[1]
-        
-        print('\n\t      ' + nombre_mes)
-        cal_formatted = '\t   ' + '\n\t   '.join(cal_lines[1:])
-        print(cal_formatted)
-    
-    elif op == '1':
-        pass
-        
-    elif op == 'x':
-        break
-    
-    else:
-        print('Opción no válida. Por favor, eliga una opción válida.')
+mostrar_calendario(year, month);
+op = input('\n\t').lower()
+if op == '0':
+   month2 = fecha_actual.month-1
+   mostrar_calendario(year, month2); 
